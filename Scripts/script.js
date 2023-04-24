70248fad22a6f3832cb171f4751369bafb521e6b
Lizzz <alejandroraidi@gmail.com>
2023-04-23 18:49:55 -0600

    User can now search a city js

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,10 +1,31 @@
+let apiKey = 'e7eee4fd93fbef038bf4a28dff8a59c7';
+let submit = document.getElementById('search-button');
 
-let weatherUrl = 'api.openweathermap.org/data/2.5/forecast?q=Toluca&appid=e7eee4fd93fbef038bf4a28dff8a59c7'; 
 
-fetch(weatherUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-    });
+let city;
+
+function searchCity(){
+    city = document.getElementById('city-input').value;
+    if(!city){
+        return
+        /*ADD A WARNING TO THE USER*/
+    }
+    startSearch();
+}
+
+function startSearch(){
+    let weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+    fetch(weatherUrl)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data)
+        })   
+}
+
+submit.addEventListener('click',searchCity)
+
+
+
